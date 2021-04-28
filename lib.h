cc741c7b5752b4fb3b36b814f84f42ae6b06a7a7
zhansayaa00 <zhans1023@hotmail.com>
2021-04-29 01:36:18 +0600

    finished

--- lib.h
+++ lib.h
@@ -1,90 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define RED 'R'
-#define BLACK 'B'
+#define RED RED
+#define BLACK BLACK
 
-typedef struct process
-{
-    unsigned int left_time;
-    unsigned int start_time;
-    unsigned int waiting_time;
-    unsigned int response_time;
-    bool runned;
-    int preempted;
-    int priority;
-
+typedef struct process {
+    unsigned int left_time; // how much time it needs to run more
+    unsigned int start_time; // when it appeared in the system
+    unsigned int waiting_time; // how much time it spent in the waiting queue
+    unsigned int response_time; // how much time it needed to be runned for the first time
+    bool runned; 
+    int preempted; //how many times it was preempted
+    int priority; 
 } process;
 
-typedef struct gclock
-{
+/* Global clock */
+typedef struct gclock{
     unsigned int ticks;
 } gclock;
 
-typedef struct RBNode
-{
-  unsigned int vruntime;
-  char color;
-  struct RBNode *parent;
-  struct RBNode *left;
-  struct RBNode *right;
-  process *process;
+/* Red-black tree properties:
+    1) Every node is either red or black.
 
+    2) Every leaf (NIL) is black.
+
+    3) If a node is red, then both its children are black.
+
+    4) Every simple path from a node to a descendant leaf contains the same number of black nodes.
+*/    
+
+typedef struct RBNode{
+    unsigned int vruntime;
+    char color;
+    struct RBNode *parent;
+    struct RBNode *left;
+    struct RBNode *right;
+    process *process;
 } RBNode;   
 
-// RBNode *root = NULL;
 RBNode nil_leaf;
 
-// A utility function to create a new BST RBNode
-RBNode* newRBNode(process* p, unsigned int vruntime)
-{
+/* create a new node and return it */
+RBNode* newRBNode(process* p, unsigned int vruntime){
     RBNode *temp   = (RBNode*) malloc(sizeof(RBNode));
     temp->vruntime = vruntime;
     temp->process = p;
+
+    /* the colour of the new node must always be red */
     temp->color  = RED;
     temp->left   = NULL;
     temp->right  = NULL;
     temp->parent = NULL;
-
     return temp;
 }
 
-void rotateLeft( RBNode** T, RBNode* x)
-{
-    RBNode *y  = x->right;    // set y
-    x->right = y->left;     // turn y's left subtree into x's right subtree{
+void rotate_left( RBNode** T, RBNode* x) {
+    RBNode *y  = x->right;    
+    x->right = y->left;     
     if (y->left != &nil_leaf)
         y->left->parent = x;
-    y->parent = x->parent;  // link x's parent to y
+    y->parent = x->parent; 
     if (x->parent == &nil_leaf)
         *T = y;
     else if (x == x->parent->left)
         x->parent->left = y;
     else
         x->parent->right = y;
-    y->left   = x;            // put x on y's left
+    y->left   = x;            
     x->parent = y;
 }
 
-void rotateRight(RBNode** T, RBNode* y)
-{
-    RBNode *x  = y->left;     // set x
-    y->left  = x->right;    // turn x's right subtree into y's left subtree{
+void rotate_right(RBNode** T, RBNode* y) {
+    RBNode *x  = y->left;     
+    y->left  = x->right;    
     if (x->right != &nil_leaf)
         x->right->parent = y;
-    x->parent = y->parent;  // link y's parent to x
+    x->parent = y->parent;  
     if (y->parent == &nil_leaf)
         *T = x;
     else if (y == y->parent->right)
         y->parent->right = x;
     else
         y->parent->left  = x;
-    x->right  = y;         // put y on x's right
+    x->right  = y;        
     y->parent = x;
 }
 
-void redBlackInsertFixup(RBNode** root, RBNode* New)
-{
+void fix_insert(RBNode** root, RBNode* New) {
     RBNode* temp;
     while (New->parent->color == RED)
     {
@@ -102,11 +104,11 @@ void redBlackInsertFixup(RBNode** root, RBNode* New)
                 if (New == New->parent->right)
                 {
                     New = New->parent;
-                    rotateLeft(root, New);
+                    rotate_left(root, New);
                 }
                 New->parent->color = BLACK;
                 New->parent->parent->color = RED;
-                rotateRight(root, New->parent->parent);
+                rotate_right(root, New->parent->parent);
             }
         }
         else
@@ -123,17 +125,18 @@ void redBlackInsertFixup(RBNode** root, RBNode* New)
                 if (New == New->parent->left)
                 {
                     New = New->parent;
-                    rotateRight(root, New);
+                    rotate_right(root, New);
                 }
                 New->parent->color = BLACK;
                 New->parent->parent->color = RED;
-                rotateLeft(root, New->parent->parent);
+                rotate_left(root, New->parent->parent);
             }
         }
     }
     root[0]->color = BLACK;
 }
 
+/* get the leftmost node */
 RBNode* left(RBNode* T) {
     RBNode* temp = T;
 
@@ -144,18 +147,17 @@ RBNode* left(RBNode* T) {
     return temp;
 } 
 
-    void rbTransplant(RBNode **root, RBNode* u, RBNode* v){
-        if (u->parent == &nil_leaf  ) {
-            *root = v;
-        } else if (u == u->parent->left){
-            u->parent->left = v;
-        } else {
-            u->parent->right = v;
-        }
-        v->parent = u->parent;
+void rbTransplant(RBNode **root, RBNode* u, RBNode* v){
+    if (u->parent == &nil_leaf  ) {
+        *root = v;
+    } else if (u == u->parent->left){
+        u->parent->left = v;
+    } else {
+        u->parent->right = v;
     }
-RBNode* redBlackInsert(RBNode** T, process* p, unsigned int vruntime)
-{
+    v->parent = u->parent;
+}
+RBNode* RB_insert(RBNode** T, process* p, unsigned int vruntime) {
     RBNode* z =  newRBNode(p, vruntime);
     RBNode* y =  &nil_leaf;
     RBNode* x = *T;
@@ -187,149 +189,110 @@ RBNode* redBlackInsert(RBNode** T, process* p, unsigned int vruntime)
     }
 
     // Ensure the Red-Black property is maintained
-    redBlackInsertFixup(T, z);
+    fix_insert(T, z);
 
     return z;
 }
 
 
 
-void fixDelete(RBNode* x, RBNode** root) {
-        RBNode* s;
-        while (x != *root && x->color == 'B') {
-            if (x == x->parent->left) {
+void fix_delete(RBNode* x, RBNode** root) {
+    RBNode* s;
+    while (x != *root && x->color == BLACK) {
+        if (x == x->parent->left) {
+            s = x->parent->right;
+            if (s->color == RED) {
+                // case 3.1
+                s->color = BLACK;
+                x->parent->color = RED;
+                rotate_left(root, x->parent);
                 s = x->parent->right;
-                if (s->color == 'R') {
-                    // case 3.1
-                    s->color = 'B';
-                    x->parent->color = 'R';
-                    rotateLeft(root, x->parent);
-                    s = x->parent->right;
-                }
+            }
 
-                if (s->left->color == 'B' && s->right->color == 'B') {
-                    // case 3.2
-                    s->color = 'R';
-                    x = x->parent;
-                } else {
-                    if (s->right->color == 'B') {
-                        // case 3.3
-                        s->left->color = 'B';
-                        s->color = 'R';
-                        rotateRight(root, s);
-                        s = x->parent->right;
-                    } 
-
-                    // case 3.4
-                    s->color = x->parent->color;
-                    x->parent->color = 'B';
-                    s->right->color = 'B';
-                    rotateLeft(root, x->parent);
-                    x = *root;
-                }
+            if (s->left->color == BLACK && s->right->color == BLACK) {
+                // case 3.2
+                s->color = RED;
+                x = x->parent;
             } else {
+                if (s->right->color == BLACK) {
+                    // case 3.3
+                    s->left->color = BLACK;
+                    s->color = RED;
+                    rotate_right(root, s);
+                    s = x->parent->right;
+                } 
+
+                // case 3.4
+                s->color = x->parent->color;
+                x->parent->color = BLACK;
+                s->right->color = BLACK;
+                rotate_left(root, x->parent);
+                x = *root;
+            }
+        } else {
+            s = x->parent->left;
+            if (s->color == RED) {
+                // case 3.1
+                s->color = BLACK;
+                x->parent->color = RED;
+                rotate_right(root, x->parent);
                 s = x->parent->left;
-                if (s->color == 'R') {
-                    // case 3.1
-                    s->color = 'B';
-                    x->parent->color = 'R';
-                    rotateRight(root, x->parent);
+            }
+
+            if (s->right->color == BLACK && s->right->color == BLACK) {
+                // case 3.2
+                s->color = RED;
+                x = x->parent;
+            } else {
+                if (s->left->color == BLACK) {
+                    // case 3.3
+                    s->right->color = BLACK;
+                    s->color = RED;
+                    rotate_left(root, s);
                     s = x->parent->left;
-                }
+                } 
 
-                if (s->right->color == 'B' && s->right->color == 'B') {
-                    // case 3.2
-                    s->color = 'R';
-                    x = x->parent;
-                } else {
-                    if (s->left->color == 'B') {
-                        // case 3.3
-                        s->right->color = 'B';
-                        s->color = 'R';
-                        rotateLeft(root, s);
-                        s = x->parent->left;
-                    } 
-
-                    // case 3.4
-                    s->color = x->parent->color;
-                    x->parent->color = 'B';
-                    s->left->color = 'B';
-                    rotateRight(root, x->parent);
-                    x = *root;
-                }
-            } 
-        }
-        x->color = 'B';
+                // case 3.4
+                s->color = x->parent->color;
+                x->parent->color = BLACK;
+                s->left->color = BLACK;
+                rotate_right(root, x->parent);
+                x = *root;
+            }
+        } 
     }
+    x->color = BLACK;
+}
 
 
 RBNode* delete(RBNode** node) {
-        // find the node containing key
-        RBNode* z = left(*node);
-        RBNode* x, *y;
-        RBNode* ans = z;
-        y = z;
-        char y_original_color = y->color;
-        if (z->left == &nil_leaf) {
-            // if(z->right == &nil_leaf || z->right == NULL){
-            //     z = &nil_leaf;
-            //     return ans;
-            // }
-            x = z->right;
-            rbTransplant(node, z, z->right);
-        }  
-        // free(z);
-        if (y_original_color == 'B'){
-            fixDelete(x, node);
-        }
-        return ans;
+    // find the node containing key
+    RBNode* z = left(*node);
+    RBNode* x, *y;
+    RBNode* ans = z;
+    y = z;
+    char y_original_color = y->color;
+    if (z->left == &nil_leaf) {
+        // if(z->right == &nil_leaf || z->right == NULL){
+        //     z = &nil_leaf;
+        //     return ans;
+        // }
+        x = z->right;
+        rbTransplant(node, z, z->right);
+    }  
+    // free(z);
+    if (y_original_color == BLACK){
+        fix_delete(x, node);
     }
+    return ans;
+}
 
-int compare(const void *s1, const void *s2)
-{
+
+int compare(const void *s1, const void *s2){
     process *p1 = (process *)s1;
     process *p2 = (process *)s2;
     if (p1->start_time == p2->start_time)
         return p1->priority < p2->priority;
     return p1->start_time > p2->start_time;
-}
-
-
-
-// #define MAX(a,b) (((a)>(b))?(a):(b))
-
-// int height(RBNode* root)
-// {
-//     int h = 0;
-
-//     if (root != NULL) {
-//         if (root == &nil_leaf)
-//             h = 1;
-//         else
-//         {
-//             int leftHeight  = height(root->left);
-//             int rightHeight = height(root->right);
-//             h = MAX(leftHeight, rightHeight) + 1;
-//         }
-//     }
-
-//     return h;
-// }
-
-// //中序遍历
-void printTree(RBNode* root)
-{
-    if(root == &nil_leaf){
-        printf("empty\n");
-        return;
-    }
-
-    if (root->left != &nil_leaf)
-        printTree(root->left);
-    fprintf(stdout, "%d %d\n", root->process->left_time, root->process->start_time);
-    fflush(stdout);
-    // printf("%d%c ", root->vruntime, root->color);
-    if (root->right != &nil_leaf)
-        printTree(root->right);
 }
 
